4bb20fee66fbf5fc4413b23a2b0de21547b0e1f6
Auzzzz <31946986+Auzzzz@users.noreply.github.com>
2022-09-22 14:09:23 +1000

    first commit

---
+++ models/user.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+  }, {});
+  // User.associate = function(models) {
+  //   User.belongsTo(models.Company, {foreignKey: 'companyId', as: 'company'})
+  // };
+  return User;
+};
