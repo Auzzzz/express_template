da62cb39db53d91985c976ad615fec4fd86655a5
Auzzzz <s3544232@student.rmit.edu.au>
2022-09-23 10:17:38 +1000

    routes & controllers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 const db = require("./models")
 const app = express();
 
@@ -12,15 +13,18 @@ app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(express.urlencoded({ extended: true }));
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+// db.sequelize.sync({ force: false }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// Routes
+require("./src/routes/test.routes")(express, app)
 
 // simple route
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
